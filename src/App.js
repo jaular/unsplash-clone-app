91baf27557c56162c7ebea0b716282af18c2e353
José <jose14aular@gmail.com>
2020-10-22 09:52:17 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Unsplash from './components/Unsplash'
 import Footer from './components/common/Footer'
 
-import './assets/css/style.scss'
+import './assets/css/style.css'
 
 function App() {
   return (
